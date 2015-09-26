9ad4ad881c77f609f8f7368661112ff0b301a900
Jack Hughes <jack@jackhughesweb.com>
2015-09-26 20:57:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# new-xkcd-slack
+
+So you're chatting in Slack, and there's a new xkcd out. Your RSS reader isn't open, and you haven't looked at [xkcd.com](http://xkcd.com) in a while. With this bot for Slack, don't worry about missing out, as it'll post the latest xkcd to Slack automagically* (*depending on the cron job that you use).
+
+## Installation
+
+1. Download this repository
+2. Set the Slack hook URL, bot name and bot image URL in xkcd-slack.py
+3. Setup a cron job to run the python script (eg. '0 0,4,8,12,16,20 * * * /path/to/new-xkcd-slack.py')
+
+## License
+
+The code is licensed under the MIT license (found in the LICENSE file).
---
+++ xkcd-slack.py
@@ -0,0 +1,52 @@
+#
+# new-xkcd-slack
+#
+# Created by Jack Hughes (github.com/jackhughesweb)
+# Licensed under the MIT License
+#
+
+import feedparser
+import os.path
+import pycurl
+import urllib
+import json
+import re
+
+## Set these
+slackhookurl = ''
+botname = ''
+boticonurl = ''
+##
+
+def newxkcd(entry):
+	xkcdtitle = entry.title
+	xkcdlink = entry.link
+	xkcddesc = entry.description
+	xkcdimg = re.search('(?<=src=")[^"]*', xkcddesc).group(0)
+	xkcdalt = re.search('(?<=alt=")[^"]*', xkcddesc).group(0)
+	c = pycurl.Curl()
+	c.setopt(c.URL, slackhookurl)
+	postdataraw = {'attachments': [{'title': xkcdtitle, 'image_url': xkcdimg}], 'icon_url': boticonurl, 'username': botname}
+	c.setopt(c.POSTFIELDS, json.dumps(postdataraw))
+	c.perform()
+	c.close()
+	c = pycurl.Curl()
+	c.setopt(c.URL, slackhookurl)
+	postdataraw = {'attachments': [{'title': xkcdtitle, 'text': 'Alt text: ' + xkcdalt}], 'icon_url': boticonurl, 'username': botname}
+	c.setopt(c.POSTFIELDS, json.dumps(postdataraw))
+	c.perform()
+	c.close()
+	newfile = open('recent.txt', 'w')
+	newfile.write(xkcdlink)
+	newfile.close()
+
+
+d = feedparser.parse('http://xkcd.com/rss.xml')
+if os.path.isfile('recent.txt'):
+	file = open('recent.txt', 'r')
+	filelink = file.readline()
+	file.close()
+	if filelink != d.entries[0].link:
+		newxkcd(d.entries[0])
+else:
+	newxkcd(d.entries[0])
